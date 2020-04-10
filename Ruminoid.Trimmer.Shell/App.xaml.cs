3a8b06334bef21aeca3940c5d6150fba8acae557
Il Harper <afanyiyu@hotmail.com>
2020-04-10 10:52:20 +0800

    feat: Add Startup Logic

--- Ruminoid.Trimmer.Shell/App.xaml.cs
+++ Ruminoid.Trimmer.Shell/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ using System.Windows;
 using System.Xml;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using MetroRadiance.UI;
+using Ruminoid.Trimmer.Shell.Windows;
 
 namespace Ruminoid.Trimmer.Shell
 {
@@ -28,6 +31,42 @@ namespace Ruminoid.Trimmer.Shell
                             HighlightingManager.Instance));
                 }
             }
+
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                args.Handled = true;
+                MessageBox.Show(
+                    args.Exception.Message,
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                MessageBox.Show(
+                    (args.ExceptionObject as Exception)?.Message ?? "Exception",
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            Shell.Properties.Resources.Culture = CultureInfo.CurrentUICulture;
+
+            StartWindow.Current.Show();
+
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeTheme(Theme.Dark));
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeAccent(Accent.Blue));
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
         }
     }
 }
