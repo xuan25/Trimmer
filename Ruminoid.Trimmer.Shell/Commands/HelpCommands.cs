b322493dcc4dd4449e01b32beab134b6bc3fce8a
Il Harper <afanyiyu@hotmail.com>
2020-04-10 23:14:01 +0800

    feat: Add Commands

---
+++ Ruminoid.Trimmer.Shell/Commands/HelpCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ruminoid.Trimmer.Shell.Commands
+{
+    public static partial class UICommands
+    {
+
+        public static RoutedUICommand DoAppUpdate { get; } = new RoutedUICommand(
+            "检查更新",
+            "DoAppUpdate",
+            typeof(UICommands),
+            new InputGestureCollection(new List<InputGesture>()));
+
+    }
+}
---
+++ Ruminoid.Trimmer.Shell/Commands/MainCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ruminoid.Trimmer.Shell.Commands
+{
+    public static partial class UICommands
+    {
+
+        public static RoutedUICommand ExitApp { get; } = new RoutedUICommand(
+            "退出",
+            "Exit",
+            typeof(UICommands),
+            new InputGestureCollection(new List<InputGesture>()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt+F4")
+            }));
+
+    }
+}
