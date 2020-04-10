f192efe15603b475631bc16c7e8656b39f6f3a60
Il Harper <afanyiyu@hotmail.com>
2020-04-10 10:22:30 +0800

    feat: Load LrcMode

--- Ruminoid.Trimmer.Shell/App.xaml.cs
+++ Ruminoid.Trimmer.Shell/App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
 namespace Ruminoid.Trimmer.Shell
 {
@@ -13,5 +17,17 @@ namespace Ruminoid.Trimmer.Shell
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            using (var stream = new MemoryStream(Shell.Properties.Resources.LrcMode))
+            {
+                using (var reader = new XmlTextReader(stream))
+                {
+                    HighlightingManager.Instance.RegisterHighlighting("Lrc", new string[0],
+                        HighlightingLoader.Load(reader,
+                            HighlightingManager.Instance));
+                }
+            }
+        }
     }
 }
