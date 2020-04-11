f849739d7ad072a6ea6b939a68135e677cc47f1d
Il Harper <afanyiyu@hotmail.com>
2020-04-11 14:14:18 +0800

    feat: Add LrcModel

---
+++ Ruminoid.Trimmer.Shell/Models/LrcModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Trimmer.Shell.Models
+{
+    public sealed class LrcModel: INotifyPropertyChanged
+    {
+
+        #region Current
+
+        public static LrcModel Current { get; set; } = new LrcModel();
+
+        #endregion
+
+        #region DataContext
+
+        private bool _isLoaded;
+
+        public bool IsLoaded
+        {
+            get => _isLoaded;
+            set
+            {
+                _isLoaded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
