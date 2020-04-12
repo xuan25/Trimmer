731f9053164878db01e84e48987a97161b384a04
Il Harper <afanyiyu@hotmail.com>
2020-04-12 13:32:15 +0800

    feat: Add Modified Class

---
+++ Ruminoid.Trimmer.Shell/Models/Modified.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Trimmer.Shell.Models
+{
+    public abstract class Modified : INotifyPropertyChanged
+    {
+
+        private bool _isModified;
+
+        public bool IsModified
+        {
+            get => _isModified;
+            set
+            {
+                _isModified = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsModified)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
