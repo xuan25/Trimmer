a8d46d56659cf9be1015038521c61c6989cb9cad
Il Harper <afanyiyu@hotmail.com>
2020-04-14 13:10:09 +0800

    feat: Add Edit/Remove Operations

--- Ruminoid.Trimmer.Shell/Views/LyricEditorViewCommandBinding.cs
+++ Ruminoid.Trimmer.Shell/Views/LyricEditorViewCommandBinding.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Ruminoid.Trimmer.Shell.Commands;
 using Ruminoid.Trimmer.Shell.Dialogs;
@@ -39,6 +40,31 @@ namespace Ruminoid.Trimmer.Shell.Views
         public void Apply() => LrcModel.Current.Apply(new Position(PlaybackView.Current.MediaPlayer.Time));
 
         public void Undo() => LrcModel.Current.Undo();
+
+        private void EditLineButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            EditLineDialog.ShowEditDialog();
+            if (string.IsNullOrEmpty(EditLineDialog.Data)) return;
+            Button s = sender as Button;
+            if (s is null) return;
+            LrcLine line = s.DataContext as LrcLine;
+            line?.ResetData(EditLineDialog.Data);
+        }
+
+        private void DeleteLineButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "删除这行？",
+                "删除",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning,
+                MessageBoxResult.No);
+            if (result != MessageBoxResult.Yes) return;
+            Button s = sender as Button;
+            if (s is null) return;
+            LrcLine line = s.DataContext as LrcLine;
+            LrcModel.Current.Items.Remove(line);
+        }
 
         #endregion
 
